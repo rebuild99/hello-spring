0bc9f565a6bbe38f83033572ee927c6840d33ac6
rebuild99 <you@example.com>
2022-03-19 12:09:52 +0900

    컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 
+@Component
 public interface MemberRepository {
 
     Member save(Member member);
